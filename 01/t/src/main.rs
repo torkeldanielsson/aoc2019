e87202a82efde0a4046cc0f96bdeb4f1b78c55a9
Torkel <torkel@voysys.se>
2019-12-02 19:15:15 +0100

    day 1

---
+++ 01/t/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let contents = fs::read_to_string(filename).expect("error reading file");
+
+    {
+        let mut sum = 0;
+
+        for line in contents.lines() {
+            let my_int = line.parse::<i32>().unwrap();
+            let val = (my_int / 3) - 2;
+            sum += val;
+        }
+
+        println!("part 1: {}", sum);
+    }
+
+    {
+        let mut total_sum = 0;
+
+        for line in contents.lines() {
+            let my_int = line.parse::<i32>().unwrap();
+            let mut val = (my_int / 3) - 2;
+            while val > 0 {
+                total_sum += val;
+                val = (val / 3) - 2;
+            }
+        }
+
+        println!("part 2: {}", total_sum);
+    }
+}
