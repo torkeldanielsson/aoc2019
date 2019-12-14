c94dc426a914b972747b8dec8964c8fbc8654c7d
torkel <torkel@voysys.se>
2019-12-14 09:44:16 +0100

    9 wip

---
+++ 09/a/src/main.rs
@@ -0,0 +1,361 @@
+use std::env;
+use std::fs;
+use std::io::Write;
+
+#[derive(Debug, PartialEq)]
+enum Mode {
+    Position,
+    Immediate,
+    Relative,
+}
+
+fn digit_to_mode(d: i32) -> Mode {
+    match d {
+        0 => Mode::Position,
+        1 => Mode::Immediate,
+        2 => Mode::Relative,
+        _ => {
+            panic!("unexpected mode digit: {}", d);
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Opcode {
+    Addition,
+    Multiplication,
+    Input,
+    Output,
+    JumpIfTrue,
+    JumpIfFalse,
+    LessThan,
+    Equals,
+    AdjustRelativeBase,
+    Break,
+}
+
+fn digit_to_opcode(d: i32) -> Opcode {
+    match d {
+        1 => Opcode::Addition,
+        2 => Opcode::Multiplication,
+        3 => Opcode::Input,
+        4 => Opcode::Output,
+        5 => Opcode::JumpIfTrue,
+        6 => Opcode::JumpIfFalse,
+        7 => Opcode::LessThan,
+        8 => Opcode::Equals,
+        9 => Opcode::AdjustRelativeBase,
+        99 => Opcode::Break,
+        _ => {
+            panic!("Illegal opcode {:?}", d);
+        }
+    }
+}
+
+fn parse_op(n: usize) -> (Opcode, Mode, Mode, Mode) {
+    fn x_inner(n: usize, xs: &mut Vec<usize>) {
+        if n >= 10 {
+            x_inner(n / 10, xs);
+        }
+        xs.push(n % 10);
+    }
+    let mut xs = Vec::new();
+    x_inner(n, &mut xs);
+
+    xs.reverse();
+
+    while xs.len() < 5 {
+        xs.push(0);
+    }
+
+    xs.reverse();
+
+    (
+        digit_to_opcode((xs[3] * 10 + xs[4]) as i32),
+        digit_to_mode(xs[0] as i32),
+        digit_to_mode(xs[1] as i32),
+        digit_to_mode(xs[2] as i32),
+    )
+}
+
+#[derive(Debug, PartialEq)]
+enum ReturnState {
+    Error,
+    NeedMoreInput,
+    ProducedOutput,
+    Break,
+}
+
+#[derive(Debug)]
+struct ProgramState {
+    program: Vec<i64>,
+    return_state: ReturnState,
+    inputs: Vec<i64>,
+    outputs: Vec<i64>,
+    pc: usize,
+    input_counter: usize,
+    relative_base: i64,
+}
+
+fn run_program(s: &mut ProgramState) {
+    while s.pc < s.program.len() {
+        let opcode = s.program[s.pc];
+
+        // println!("s.pc {:?} opcode {:?}", s.pc, opcode);
+
+        let (op, mode3, mode2, mode1) = parse_op(opcode as usize);
+
+        let val1 = s.program[s.pc + 1];
+        let val2 = s.program[s.pc + 2];
+        let val3 = s.program[s.pc + 3];
+
+        // println!(
+        //     "op: {:?}, mode1: {:?}, mode2: {:?}, mode3: {:?}, val1: {:?}, val2: {:?}, val3: {:?}",
+        //     op, mode1, mode2, mode3, val1, val2, val3
+        // );
+
+        match op {
+            Opcode::Addition => {
+                let a = match mode1 {
+                    Mode::Position => s.program[val1 as usize],
+                    Mode::Immediate => val1,
+                    Mode::Relative => s.program[(s.relative_base + val1) as usize],
+                };
+
+                let b = match mode2 {
+                    Mode::Position => s.program[val2 as usize],
+                    Mode::Immediate => val2,
+                    Mode::Relative => s.program[(s.relative_base + val2) as usize],
+                };
+
+                if mode3 == Mode::Immediate {
+                    panic!("illegal immediate mode");
+                }
+
+                match mode3 {
+                    Mode::Position => s.program[val1 as usize] = a + b,
+                    Mode::Immediate => panic!("illegal immediate mode"),
+                    Mode::Relative => s.program[(s.relative_base + val2) as usize] = a + b,
+                };
+
+                s.pc += 4;
+            }
+            Opcode::Multiplication => {
+                let a = match mode1 {
+                    Mode::Position => s.program[val1 as usize],
+                    Mode::Immediate => val1,
+                    Mode::Relative => s.program[(s.relative_base + val1) as usize],
+                };
+
+                let b = match mode2 {
+                    Mode::Position => s.program[val2 as usize],
+                    Mode::Immediate => val2,
+                    Mode::Relative => s.program[(s.relative_base + val2) as usize],
+                };
+
+                match mode3 {
+                    Mode::Position => s.program[val1 as usize] = a * b,
+                    Mode::Immediate => panic!("illegal immediate mode"),
+                    Mode::Relative => s.program[(s.relative_base + val2) as usize] = a * b,
+                };
+
+                s.pc += 4;
+            }
+            Opcode::Input => {
+                if s.inputs.len() <= s.input_counter {
+                    s.return_state = ReturnState::NeedMoreInput;
+                    return;
+                }
+
+                match mode1 {
+                    Mode::Position => s.program[val1 as usize] = s.inputs[s.input_counter],
+                    Mode::Immediate => panic!("illegal immediate mode"),
+                    Mode::Relative => {
+                        s.program[(s.relative_base + val2) as usize] = s.inputs[s.input_counter]
+                    }
+                };
+
+                s.input_counter += 1;
+
+                s.pc += 2;
+            }
+            Opcode::Output => {
+                if mode1 == Mode::Position {
+                    s.outputs.push(s.program[val1 as usize])
+                } else {
+                    s.outputs.push(val1)
+                }
+
+                s.pc += 2;
+
+                s.return_state = ReturnState::ProducedOutput;
+                return;
+            }
+            Opcode::JumpIfTrue => {
+                let a = match mode1 {
+                    Mode::Position => s.program[val1 as usize],
+                    Mode::Immediate => val1,
+                    Mode::Relative => s.program[(s.relative_base + val1) as usize],
+                };
+
+                let b = match mode2 {
+                    Mode::Position => s.program[val2 as usize],
+                    Mode::Immediate => val2,
+                    Mode::Relative => s.program[(s.relative_base + val2) as usize],
+                };
+
+                if a != 0 {
+                    s.pc = b as usize;
+                } else {
+                    s.pc += 3;
+                }
+            }
+            Opcode::JumpIfFalse => {
+                let a = match mode1 {
+                    Mode::Position => s.program[val1 as usize],
+                    Mode::Immediate => val1,
+                    Mode::Relative => s.program[(s.relative_base + val1) as usize],
+                };
+
+                let b = match mode2 {
+                    Mode::Position => s.program[val2 as usize],
+                    Mode::Immediate => val2,
+                    Mode::Relative => s.program[(s.relative_base + val2) as usize],
+                };
+
+                if a == 0 {
+                    s.pc = b as usize;
+                } else {
+                    s.pc += 3;
+                }
+            }
+            Opcode::LessThan => {
+                let a = match mode1 {
+                    Mode::Position => s.program[val1 as usize],
+                    Mode::Immediate => val1,
+                    Mode::Relative => s.program[(s.relative_base + val1) as usize],
+                };
+
+                let b = match mode2 {
+                    Mode::Position => s.program[val2 as usize],
+                    Mode::Immediate => val2,
+                    Mode::Relative => s.program[(s.relative_base + val2) as usize],
+                };
+
+                if mode3 == Mode::Immediate {
+                    panic!("illegal immediate mode");
+                }
+
+                if a < b {
+                    s.program[val3 as usize] = 1;
+                } else {
+                    s.program[val3 as usize] = 0;
+                }
+
+                s.pc += 4;
+            }
+            Opcode::Equals => {
+                let a = match mode1 {
+                    Mode::Position => s.program[val1 as usize],
+                    Mode::Immediate => val1,
+                    Mode::Relative => s.program[(s.relative_base + val1) as usize],
+                };
+
+                let b = match mode2 {
+                    Mode::Position => s.program[val2 as usize],
+                    Mode::Immediate => val2,
+                    Mode::Relative => s.program[(s.relative_base + val2) as usize],
+                };
+
+                if mode3 == Mode::Immediate {
+                    panic!("illegal immediate mode");
+                }
+
+                if a == b {
+                    s.program[val3 as usize] = 1;
+                } else {
+                    s.program[val3 as usize] = 0;
+                }
+
+                s.pc += 4;
+            }
+            Opcode::AdjustRelativeBase => {
+                let a = match mode1 {
+                    Mode::Position => s.program[val1 as usize],
+                    Mode::Immediate => val1,
+                    Mode::Relative => s.program[(s.relative_base + val1) as usize],
+                };
+
+                s.relative_base += a;
+
+                s.pc += 2;
+            }
+            Opcode::Break => {
+                s.return_state = ReturnState::Break;
+                return;
+            }
+        }
+    }
+}
+
+fn parse_program(input: &str) -> Vec<i64> {
+    let mut program: Vec<i64> = input
+        .split(',')
+        .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect();
+
+    program.push(0);
+    program.push(0);
+    program.push(0);
+
+    program
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let input = fs::read_to_string(filename).expect("error reading file");
+
+    let mut program = ProgramState {
+        program: parse_program(&input),
+        return_state: ReturnState::Error,
+        inputs: vec![],
+        outputs: vec![],
+        pc: 0,
+        input_counter: 0,
+        relative_base: 0,
+    };
+
+    run_program(&mut program);
+
+    println!("{:?}", program);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_run() {
+        {
+            let input = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99";
+            let mut program = ProgramState {
+                program: parse_program(&input),
+                return_state: ReturnState::Error,
+                inputs: vec![],
+                outputs: vec![],
+                pc: 0,
+                input_counter: 0,
+                relative_base: 0,
+            };
+
+            run_program(&mut program);
+
+            let expected_result = 139629729;
+            assert_eq!(expected_result, program.outputs[0]);
+        }
+    }
+}
