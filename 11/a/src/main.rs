b8d342c75e8f2033d91a64e6d9be9c9b1c30dd49
torkel <torkel@voysys.se>
2019-12-15 22:32:34 +0100

    11a

---
+++ 11/a/src/main.rs
@@ -0,0 +1,501 @@
+#[derive(Debug, PartialEq)]
+enum Mode {
+    Position,
+    Immediate,
+    Relative,
+}
+
+fn digit_to_mode(d: i32) -> Mode {
+    match d {
+        0 => Mode::Position,
+        1 => Mode::Immediate,
+        2 => Mode::Relative,
+        _ => {
+            panic!("unexpected mode digit: {}", d);
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Opcode {
+    Addition,
+    Multiplication,
+    Input,
+    Output,
+    JumpIfTrue,
+    JumpIfFalse,
+    LessThan,
+    Equals,
+    AdjustRelativeBase,
+    Break,
+}
+
+fn digit_to_opcode(d: i32) -> Opcode {
+    match d {
+        1 => Opcode::Addition,
+        2 => Opcode::Multiplication,
+        3 => Opcode::Input,
+        4 => Opcode::Output,
+        5 => Opcode::JumpIfTrue,
+        6 => Opcode::JumpIfFalse,
+        7 => Opcode::LessThan,
+        8 => Opcode::Equals,
+        9 => Opcode::AdjustRelativeBase,
+        99 => Opcode::Break,
+        _ => {
+            panic!("Illegal opcode {:?}", d);
+        }
+    }
+}
+
+fn parse_op(n: usize) -> (Opcode, Mode, Mode, Mode) {
+    fn x_inner(n: usize, xs: &mut Vec<usize>) {
+        if n >= 10 {
+            x_inner(n / 10, xs);
+        }
+        xs.push(n % 10);
+    }
+    let mut xs = Vec::new();
+    x_inner(n, &mut xs);
+
+    xs.reverse();
+
+    while xs.len() < 5 {
+        xs.push(0);
+    }
+
+    xs.reverse();
+
+    (
+        digit_to_opcode((xs[3] * 10 + xs[4]) as i32),
+        digit_to_mode(xs[0] as i32),
+        digit_to_mode(xs[1] as i32),
+        digit_to_mode(xs[2] as i32),
+    )
+}
+
+#[derive(Debug, PartialEq)]
+enum ReturnState {
+    NeedMoreInput,
+    ProducedOutput,
+    Break,
+}
+
+#[derive(Debug)]
+struct ProgramState {
+    program: Vec<i64>,
+    return_state: ReturnState,
+    inputs: Vec<i64>,
+    outputs: Vec<i64>,
+    pc: usize,
+    input_counter: usize,
+    relative_base: i64,
+}
+
+fn run_program(s: &mut ProgramState) {
+    while s.pc < s.program.len() {
+        let opcode = s.program[s.pc];
+
+        // println!("s.pc {:?} opcode {:?}", s.pc, opcode);
+
+        let (op, mode3, mode2, mode1) = parse_op(opcode as usize);
+
+        let val1 = s.program[s.pc + 1];
+        let val2 = s.program[s.pc + 2];
+        let val3 = s.program[s.pc + 3];
+
+        // println!(
+        //     "op: {:?}, mode1: {:?}, mode2: {:?}, mode3: {:?}, val1: {:?}, val2: {:?}, val3: {:?}, a: {}, b: {}",
+        //     op, mode1, mode2, mode3, val1, val2, val3, a, b
+        // );
+
+        match op {
+            Opcode::Addition => {
+                if mode3 == Mode::Immediate {
+                    panic!("illegal immediate mode");
+                }
+
+                let a = match mode1 {
+                    Mode::Position => s.program[val1 as usize],
+                    Mode::Immediate => val1,
+                    Mode::Relative => s.program[(s.relative_base + val1) as usize],
+                };
+
+                let b = match mode2 {
+                    Mode::Position => s.program[val2 as usize],
+                    Mode::Immediate => val2,
+                    Mode::Relative => s.program[(s.relative_base + val2) as usize],
+                };
+
+                match mode3 {
+                    Mode::Position => s.program[val3 as usize] = a + b,
+                    Mode::Immediate => panic!("illegal immediate mode"),
+                    Mode::Relative => s.program[(s.relative_base + val3) as usize] = a + b,
+                };
+
+                s.pc += 4;
+            }
+            Opcode::Multiplication => {
+                let a = match mode1 {
+                    Mode::Position => s.program[val1 as usize],
+                    Mode::Immediate => val1,
+                    Mode::Relative => s.program[(s.relative_base + val1) as usize],
+                };
+
+                let b = match mode2 {
+                    Mode::Position => s.program[val2 as usize],
+                    Mode::Immediate => val2,
+                    Mode::Relative => s.program[(s.relative_base + val2) as usize],
+                };
+
+                match mode3 {
+                    Mode::Position => s.program[val3 as usize] = a * b,
+                    Mode::Immediate => panic!("illegal immediate mode"),
+                    Mode::Relative => s.program[(s.relative_base + val3) as usize] = a * b,
+                };
+
+                s.pc += 4;
+            }
+            Opcode::Input => {
+                if s.inputs.len() <= s.input_counter {
+                    s.return_state = ReturnState::NeedMoreInput;
+                    return;
+                }
+
+                match mode1 {
+                    Mode::Position => s.program[val1 as usize] = s.inputs[s.input_counter],
+                    Mode::Immediate => panic!("illegal immediate mode"),
+                    Mode::Relative => {
+                        s.program[(s.relative_base + val1) as usize] = s.inputs[s.input_counter]
+                    }
+                };
+
+                s.input_counter += 1;
+
+                s.pc += 2;
+            }
+            Opcode::Output => {
+                let a = match mode1 {
+                    Mode::Position => s.program[val1 as usize],
+                    Mode::Immediate => val1,
+                    Mode::Relative => s.program[(s.relative_base + val1) as usize],
+                };
+                s.outputs.push(a);
+
+                s.pc += 2;
+
+                s.return_state = ReturnState::ProducedOutput;
+                return;
+            }
+            Opcode::JumpIfTrue => {
+                let a = match mode1 {
+                    Mode::Position => s.program[val1 as usize],
+                    Mode::Immediate => val1,
+                    Mode::Relative => s.program[(s.relative_base + val1) as usize],
+                };
+
+                let b = match mode2 {
+                    Mode::Position => s.program[val2 as usize],
+                    Mode::Immediate => val2,
+                    Mode::Relative => s.program[(s.relative_base + val2) as usize],
+                };
+
+                if a != 0 {
+                    s.pc = b as usize;
+                } else {
+                    s.pc += 3;
+                }
+            }
+            Opcode::JumpIfFalse => {
+                let a = match mode1 {
+                    Mode::Position => s.program[val1 as usize],
+                    Mode::Immediate => val1,
+                    Mode::Relative => s.program[(s.relative_base + val1) as usize],
+                };
+
+                let b = match mode2 {
+                    Mode::Position => s.program[val2 as usize],
+                    Mode::Immediate => val2,
+                    Mode::Relative => s.program[(s.relative_base + val2) as usize],
+                };
+
+                if a == 0 {
+                    s.pc = b as usize;
+                } else {
+                    s.pc += 3;
+                }
+            }
+            Opcode::LessThan => {
+                let a = match mode1 {
+                    Mode::Position => s.program[val1 as usize],
+                    Mode::Immediate => val1,
+                    Mode::Relative => s.program[(s.relative_base + val1) as usize],
+                };
+
+                let b = match mode2 {
+                    Mode::Position => s.program[val2 as usize],
+                    Mode::Immediate => val2,
+                    Mode::Relative => s.program[(s.relative_base + val2) as usize],
+                };
+
+                let c_index = match mode3 {
+                    Mode::Position => val3 as usize,
+                    Mode::Immediate => panic!("illegal immediate mode"),
+                    Mode::Relative => (s.relative_base + val3) as usize,
+                };
+
+                if a < b {
+                    s.program[c_index] = 1;
+                } else {
+                    s.program[c_index] = 0;
+                }
+
+                s.pc += 4;
+            }
+            Opcode::Equals => {
+                let a = match mode1 {
+                    Mode::Position => s.program[val1 as usize],
+                    Mode::Immediate => val1,
+                    Mode::Relative => s.program[(s.relative_base + val1) as usize],
+                };
+
+                let b = match mode2 {
+                    Mode::Position => s.program[val2 as usize],
+                    Mode::Immediate => val2,
+                    Mode::Relative => s.program[(s.relative_base + val2) as usize],
+                };
+
+                let c_index = match mode3 {
+                    Mode::Position => val3 as usize,
+                    Mode::Immediate => panic!("illegal immediate mode"),
+                    Mode::Relative => (s.relative_base + val3) as usize,
+                };
+
+                if a == b {
+                    s.program[c_index] = 1;
+                } else {
+                    s.program[c_index] = 0;
+                }
+
+                s.pc += 4;
+            }
+            Opcode::AdjustRelativeBase => {
+                let a = match mode1 {
+                    Mode::Position => s.program[val1 as usize],
+                    Mode::Immediate => val1,
+                    Mode::Relative => s.program[(s.relative_base + val1) as usize],
+                };
+
+                s.relative_base += a;
+
+                s.pc += 2;
+            }
+            Opcode::Break => {
+                s.return_state = ReturnState::Break;
+                return;
+            }
+        }
+    }
+}
+
+fn parse_program(input: &str) -> Vec<i64> {
+    let mut program: Vec<i64> = input
+        .split(',')
+        .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect();
+
+    program.resize(1000000, 0);
+
+    program
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Color {
+    White,
+    Black,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+struct Vec2 {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+struct PaintedSpot {
+    pos: Vec2,
+    color: Color,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+struct State {
+    paint_info: Vec<PaintedSpot>,
+    pos: Vec2,
+    direction: Direction,
+}
+
+fn print_state(state: &State) {
+    let mut text: String = "".to_string();
+
+    let mut max = Vec2 {
+        x: -999999,
+        y: -9999999,
+    };
+    let mut min = Vec2 {
+        x: 999999,
+        y: 9999999,
+    };
+    for pi in &state.paint_info {
+        if max.x < pi.pos.x {
+            max.x = pi.pos.x;
+        }
+        if max.y < pi.pos.y {
+            max.y = pi.pos.y;
+        }
+        if min.x > pi.pos.x {
+            min.x = pi.pos.x;
+        }
+        if min.y > pi.pos.y {
+            min.y = pi.pos.y;
+        }
+    }
+
+    for x in min.x..=max.x {
+        for y in min.y..=max.y {
+            if state.pos.x == x && state.pos.y == y {
+                match state.direction {
+                    Direction::Up => text.push_str("^"),
+                    Direction::Right => text.push_str(">"),
+                    Direction::Down => text.push_str("v"),
+                    Direction::Left => text.push_str("<"),
+                }
+            } else {
+                let mut found = false;
+                for pi in &state.paint_info {
+                    if pi.pos.x == x && pi.pos.y == y {
+                        found = true;
+                        match pi.color {
+                            Color::White => text.push_str("#"),
+                            Color::Black => text.push_str("."),
+                        };
+                    }
+                }
+                if !found {
+                    text.push_str(".")
+                }
+            }
+        }
+        text.push_str("\n");
+    }
+    text.push_str("\n");
+    text.push_str("\n");
+    println!("{}", text);
+}
+
+fn main() {
+    let input = "3,8,1005,8,290,1106,0,11,0,0,0,104,1,104,0,3,8,1002,8,-1,10,1001,10,1,10,4,10,108,1,8,10,4,10,1002,8,1,28,1006,0,59,3,8,1002,8,-1,10,101,1,10,10,4,10,108,0,8,10,4,10,101,0,8,53,3,8,1002,8,-1,10,101,1,10,10,4,10,1008,8,0,10,4,10,101,0,8,76,1006,0,81,1,1005,2,10,3,8,102,-1,8,10,1001,10,1,10,4,10,1008,8,1,10,4,10,1002,8,1,105,3,8,102,-1,8,10,1001,10,1,10,4,10,108,1,8,10,4,10,1001,8,0,126,3,8,1002,8,-1,10,1001,10,1,10,4,10,108,1,8,10,4,10,1002,8,1,148,3,8,102,-1,8,10,101,1,10,10,4,10,1008,8,1,10,4,10,1001,8,0,171,3,8,1002,8,-1,10,1001,10,1,10,4,10,1008,8,0,10,4,10,101,0,8,193,1,1008,8,10,1,106,3,10,1006,0,18,3,8,1002,8,-1,10,1001,10,1,10,4,10,108,0,8,10,4,10,1001,8,0,225,1,1009,9,10,1006,0,92,3,8,1002,8,-1,10,1001,10,1,10,4,10,108,0,8,10,4,10,1001,8,0,254,2,1001,8,10,1,106,11,10,2,102,13,10,1006,0,78,101,1,9,9,1007,9,987,10,1005,10,15,99,109,612,104,0,104,1,21102,1,825594852136,1,21101,0,307,0,1106,0,411,21101,0,825326580628,1,21101,0,318,0,1105,1,411,3,10,104,0,104,1,3,10,104,0,104,0,3,10,104,0,104,1,3,10,104,0,104,1,3,10,104,0,104,0,3,10,104,0,104,1,21102,179557207043,1,1,21101,0,365,0,1106,0,411,21101,0,46213012483,1,21102,376,1,0,1106,0,411,3,10,104,0,104,0,3,10,104,0,104,0,21101,988648727316,0,1,21102,399,1,0,1105,1,411,21102,988224959252,1,1,21101,0,410,0,1106,0,411,99,109,2,21201,-1,0,1,21101,0,40,2,21102,1,442,3,21101,432,0,0,1105,1,475,109,-2,2105,1,0,0,1,0,0,1,109,2,3,10,204,-1,1001,437,438,453,4,0,1001,437,1,437,108,4,437,10,1006,10,469,1102,0,1,437,109,-2,2105,1,0,0,109,4,2102,1,-1,474,1207,-3,0,10,1006,10,492,21101,0,0,-3,21202,-3,1,1,22102,1,-2,2,21101,0,1,3,21102,511,1,0,1105,1,516,109,-4,2105,1,0,109,5,1207,-3,1,10,1006,10,539,2207,-4,-2,10,1006,10,539,21201,-4,0,-4,1106,0,607,21202,-4,1,1,21201,-3,-1,2,21202,-2,2,3,21101,558,0,0,1106,0,516,22101,0,1,-4,21101,1,0,-1,2207,-4,-2,10,1006,10,577,21102,1,0,-1,22202,-2,-1,-2,2107,0,-3,10,1006,10,599,21201,-1,0,1,21101,0,599,0,105,1,474,21202,-2,-1,-2,22201,-4,-2,-4,109,-5,2106,0,0";
+    let mut program = ProgramState {
+        program: parse_program(&input),
+        return_state: ReturnState::ProducedOutput,
+        inputs: vec![],
+        outputs: vec![],
+        pc: 0,
+        input_counter: 0,
+        relative_base: 0,
+    };
+
+    let mut state = State {
+        paint_info: Vec::new(),
+        pos: Vec2 { x: 0, y: 0 },
+        direction: Direction::Up,
+    };
+
+    while program.return_state != ReturnState::Break {
+        run_program(&mut program);
+
+        if program.return_state == ReturnState::NeedMoreInput {
+            let mut found = false;
+            for pi in &state.paint_info {
+                if pi.pos == state.pos {
+                    found = true;
+                    program.inputs.push(match pi.color {
+                        Color::White => 1,
+                        Color::Black => 0,
+                    });
+                }
+            }
+            if !found {
+                program.inputs.push(0);
+            }
+        }
+
+        if program.return_state == ReturnState::ProducedOutput && program.outputs.len() != 0 {
+            if program.outputs.len() % 2 == 1 {
+                let mut found = false;
+                for pi in &mut state.paint_info {
+                    if pi.pos == state.pos {
+                        found = true;
+
+                        pi.color = match program.outputs.last().unwrap() {
+                            0 => Color::Black,
+                            1 => Color::White,
+                            _ => panic!("wrong assumption"),
+                        };
+                    }
+                }
+                if !found {
+                    match program.outputs.last().unwrap() {
+                        0 => {
+                            state.paint_info.push(PaintedSpot {
+                                pos: state.pos.clone(),
+                                color: Color::Black,
+                            });
+                        }
+                        1 => state.paint_info.push(PaintedSpot {
+                            pos: state.pos.clone(),
+                            color: Color::White,
+                        }),
+                        _ => panic!("wrong assumption"),
+                    }
+                }
+            } else {
+                match program.outputs.last().unwrap() {
+                    0 => match state.direction {
+                        Direction::Up => state.direction = Direction::Left,
+                        Direction::Right => state.direction = Direction::Up,
+                        Direction::Down => state.direction = Direction::Right,
+                        Direction::Left => state.direction = Direction::Down,
+                    },
+                    1 => match state.direction {
+                        Direction::Up => state.direction = Direction::Right,
+                        Direction::Right => state.direction = Direction::Down,
+                        Direction::Down => state.direction = Direction::Left,
+                        Direction::Left => state.direction = Direction::Up,
+                    },
+                    _ => panic!("wrong assumption"),
+                }
+
+                match state.direction {
+                    Direction::Up => state.pos.y += 1,
+                    Direction::Right => state.pos.x += 1,
+                    Direction::Down => state.pos.y -= 1,
+                    Direction::Left => state.pos.x -= 1,
+                }
+            }
+        }
+    }
+
+    print_state(&state);
+
+    println!("length of state.paint_info: {}", state.paint_info.len());
+}
